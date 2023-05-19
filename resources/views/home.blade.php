88bd0fced68a4458b104a2fd1c3338fd05bb2ec1
AndreaCollura <andrea.collur@gmail.com>
2023-05-19 17:50:09 +0200

    add task/staff pages

---
+++ resources/views/home.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+    </style>
+</head>
+
+<body class="antialiased">
+    <h1>{{ $page_title }}</h1>
+    <div>
+        <a href="{{ route('staff') }}">Go to Staff</a>
+    </div>
+    <div>
+        <a href="{{ route('task') }}">Go to Tasks</a>
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        'page_title' => 'Schedule',
+        'description' => 'Staff tasks manager',
+
+    ];
+    return view('home', $data);
 });
+
+Route::get('/staff', function () {
+    $page_title = 'Staff';
+    $staff =  [
+        'Michael Jackson',
+        'Bruce Willice',
+        'Margot Robbie',
+        'Leonardo DiCaprio',
+    ];
+    return view('staff', compact('page_title', 'staff'));
+})->name('staff');
+
+Route::get('/task', function () {
+    $page_title = 'Task';
+    $tasks = [
+        'orders',
+        'customer services',
+        'front office',
+        'back office',
+    ];
+    return view('task', compact('page_title' , 'tasks'));
+})->name('task');
